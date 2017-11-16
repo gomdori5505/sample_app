2c5cf19bc23df99bdb0fcb9cc1e9db840c5f4315
gomdori5505 <gomdori5505@nate.com>
2017-11-16 20:51:32 +0900

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108094624) do
+ActiveRecord::Schema.define(version: 20171116105842) do
 
   create_table "users", force: true do |t|
     t.string   "name"
